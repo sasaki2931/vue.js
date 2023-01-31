9260163ad8302a09d2c8b3d68c675085deb5fd2e
syunsuke sasaki <sasaki2931@gmail.com>
2023-02-01 00:50:30 +0900

    エラー修正

--- sample.js
+++ sample.js
@@ -7,7 +7,7 @@ new Vue({
       course: 'Webエンジニアコース',
       acceptancePeriod: '2019年01月期',
       defaultLastId: 3,
-      tasks: [
+      students: [
         { id: 1, name: '野呂浩良', course: '機械学習コース', acceptancePeriod: '2019年01月期' },
         { id: 2, name: '富永修司', course: 'Webエンジニアコース', acceptancePeriod: '2017年11月期' },
         { id: 3, name: '斉藤一起', course: 'Webエンジニアコース', acceptancePeriod: '2017年11月期' }
